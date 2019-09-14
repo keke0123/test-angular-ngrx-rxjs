7e40547a6e44ad12fc70b98ea1323848190e1727
keke0123 <keke0123@naver.com>
2019-09-14 09:18:06 +0900

    구독 취소하는 방식 자동으로 바꿈

--- src/app/page/keep-alive/keep-alive.component.ts
+++ src/app/page/keep-alive/keep-alive.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NavigationEnd, NavigationStart, Router} from '@angular/router';
-import {Observable, fromEvent, Subscriber} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
+import {Observable, fromEvent, Subscriber, Subject} from 'rxjs';
+import {filter, map, takeUntil} from 'rxjs/operators';
 import {Store} from '@ngrx/store';
 
 import * as fromTabStore from '../../store/tab/reducers';
@@ -20,6 +20,9 @@ export class KeepAliveComponent implements OnInit {
   public url: string;
   // public tab$: Observable<any>;
   public activeTab;
+  public tabSubscriber$;
+  // destroy
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private router: Router,
@@ -39,18 +42,22 @@ export class KeepAliveComponent implements OnInit {
     // this.activeTab = this.store.select(fromTabStore.tabStoreFeatureKey, 'tab');
     // console.log('active tab', this.activeTab);
     // 아래처럼 구독 해놓고 계속 subscribe 로 연결시켜주면 store 의 값이 업데이트될때마다 실행되는듯 하다.
-    let test = this.store.select(fromTabStore.tabStoreFeatureKey, 'tab')
+    this.store.select(fromTabStore.tabStoreFeatureKey, 'tab', 'activeTab')
+      .pipe(
+        takeUntil(this.destroy$),
+      )
       .subscribe((val) => {
         console.log('val', val);
       });
-    this.store.dispatch(new tabActions.LoadTabs());
+    // this.store.dispatch(new tabActions.LoadTabs());
     // activeTab from store
 
     // url
-    this.scopeUrl$ = this.router.events.pipe(
+    this.router.events.pipe(
       filter((val) => {
         return val instanceof NavigationEnd;
       })
+      ,takeUntil(this.destroy$)
     )
       .subscribe((val) => {
         console.log(val['url']);
@@ -63,7 +70,11 @@ export class KeepAliveComponent implements OnInit {
 
   ngOnDestroy() {
     console.log('destroy');
-    this.scopeUrl$.unsubscribe();
+    // this.scopeUrl$.unsubscribe();
+    // this.tabSubscriber$.unsubscribe();
+    // destroy / 이런식으로 만들어 주면 될듯 하다. 아무래도 찌꺼기가 남을거 같긴 하지만
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
   changeTabs(tab) {
